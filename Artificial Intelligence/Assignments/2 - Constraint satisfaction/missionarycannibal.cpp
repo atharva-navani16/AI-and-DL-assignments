7b852e50edc3c89fa30c0bd5c2cf6e900991bcf5
atharva-navani16 <atharvan46@gmail.com>
2024-11-24 10:01:13 +0530

    Update missionarycannibal.cpp

--- Artificial Intelligence/Assignments/2 - Constraint satisfaction/missionarycannibal.cpp
+++ Artificial Intelligence/Assignments/2 - Constraint satisfaction/missionarycannibal.cpp
@@ -1,100 +1,80 @@
-#include <iostream>
-#include <iomanip>
+# Initialize variables
+im, ic, fm, fc, flag, select = 3, 3, 0, 0, 0, 0
 
-using namespace std;
+def display(bpass1, bpass2):
+    global im, ic, fm, fc, flag
+    print("\n\n\n")
+    print(" M " * fm + " C " * fc, end="")
+    if flag == 0:
+        print(f"     __________WATER___________BO({bpass1},{bpass2})AT  ", end="")
+    else:
+        print(f"     BO({bpass1},{bpass2})AT__________WATER___________  ", end="")
+    print(" M " * im + " C " * ic)
 
-int im = 3, ic = 3, fm = 0, fc = 0, flag = 0, select = 0;
+def win():
+    global fc, fm
+    return not (fc == 3 and fm == 3)
 
-void display(char bpass1, char bpass2) {
-    cout << "\n\n\n";
-    for (int i = 0; i < fm; i++) {
-        cout << " M ";
-    }
-    for (int i = 0; i < fc; i++) {
-        cout << " C ";
-    }
-    if (flag == 0)
-        cout << "     __________WATER___________BO(" << bpass1 << "," << bpass2 << ")AT  ";
-    else
-        cout << "     BO(" << bpass1 << "," << bpass2 << ")AT__________WATER___________  ";
-    for (int i = 0; i < im; i++) {
-        cout << " M ";
-    }
-    for (int i = 0; i < ic; i++) {
-        cout << " C ";
-    }
-}
+def solution():
+    global im, ic, fm, fc, flag, select
 
-bool win() {
-    return !(fc == 3 && fm == 3);
-}
+    while win():
+        if flag == 0:  # Boat on the starting side
+            if select == 1:
+                display('C', ' ')
+                ic += 1
+            elif select == 2:
+                display('C', 'M')
+                ic += 1
+                im += 1
 
-void solution() {
-    while (win()) {
-        if (flag == 0) {
-            switch (select) {
-                case 1: display('C', ' ');
-                    ic++;
-                    break;
-                case 2: display('C', 'M');
-                    ic++;
-                    im++;
-                    break;
-            }
-            if (((im - 2) >= ic && (fm + 2) >= fc) || (im - 2) == 0) {
-                im -= 2;
-                select = 1;
-                display('M', 'M');
-                flag = 1;
-            } else if ((ic - 2) < im && (fm == 0 || (fc + 2) <= fm) || im == 0) {
-                ic -= 2;
-                select = 2;
-                display('C', 'C');
-                flag = 1;
-            } else if ((ic - 1) <= (im - 1) && (fm + 1) >= (fc + 1)) {
-                ic -= 1;
-                im -= 1;
-                select = 3;
-                display('M', 'C');
-                flag = 1;
-            }
-        } else {
-            switch (select) {
-                case 1: display('M', 'M');
-                    fm += 2;
-                    break;
-                case 2: display('C', 'C');
-                    fc += 2;
-                    break;
-                case 3: display('M', 'C');
-                    fc += 1;
-                    fm += 1;
-                    break;
-            }
-            if (win()) {
-                if ((fc > 1 && fm == 0) || im == 0) {
-                    fc--;
-                    select = 1;
-                    display('C', ' ');
-                    flag = 0;
-                } else if ((ic + 2) > im) {
-                    fc--;
-                    fm--;
-                    select = 2;
-                    display('C', 'M');
-                    flag = 0;
-                }
-            }
-        }
-    }
-}
+            if ((im - 2) >= ic and (fm + 2) >= fc) or (im - 2) == 0:
+                im -= 2
+                select = 1
+                display('M', 'M')
+                flag = 1
+            elif (ic - 2) < im and (fm == 0 or (fc + 2) <= fm) or im == 0:
+                ic -= 2
+                select = 2
+                display('C', 'C')
+                flag = 1
+            elif (ic - 1) <= (im - 1) and (fm + 1) >= (fc + 1):
+                ic -= 1
+                im -= 1
+                select = 3
+                display('M', 'C')
+                flag = 1
+        else:  # Boat on the other side
+            if select == 1:
+                display('M', 'M')
+                fm += 2
+            elif select == 2:
+                display('C', 'C')
+                fc += 2
+            elif select == 3:
+                display('M', 'C')
+                fc += 1
+                fm += 1
 
-int main() {
-    cout << "MISSIONARIES AND CANNIBALS";
-    display(' ', ' ');
-    solution();
-    display(' ', ' ');
-    cout << "\n\n";
+            if win():
+                if (fc > 1 and fm == 0) or im == 0:
+                    fc -= 1
+                    select = 1
+                    display('C', ' ')
+                    flag = 0
+                elif (ic + 2) > im:
+                    fc -= 1
+                    fm -= 1
+                    select = 2
+                    display('C', 'M')
+                    flag = 0
 
-    return 0;
-}
+def main():
+    print("MISSIONARIES AND CANNIBALS")
+    display(' ', ' ')
+    solution()
+    display(' ', ' ')
+    print("\n\n")
+
+if __name__ == "__main__":
+    main()
